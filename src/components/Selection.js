fff04c6c95cde3f095433d74bf71368d105813cb
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-12-10 11:58:49 +0530

    initial changes

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
+import React, { useState } from "react";
+import "../styles/Child.css";
 
+export default function Selection(props) {
+  const [color, setColor] = useState({ background: "" });
+  const { applyColor } = props;
+  return (
+    <>
+      <div
+        className="container"
+        style={color}
+        onClick={() => applyColor(setColor)}
+      >
+        <h2 className="subheading">Selection</h2>
+      </div>
+    </>
+  );
+}
